8aa227f3e9fb6b026824dd978610ff42a6a4c935
Pierre <pierredureau@yahoo.fr>
2024-08-08 11:06:11 +0200

    Add UI Patterns 2.x plugins

---
+++ modules/ui_icons_patterns/src/Plugin/UiPatterns/Source/IconRenderableSource.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ui_icons_patterns\Plugin\UiPatterns\Source;
+
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\ui_patterns\Attribute\Source;
+
+/**
+ * Plugin implementation of the source.
+ */
+#[Source(
+  id: 'icon_renderable',
+  label: new TranslatableMarkup('Icon'),
+  description: new TranslatableMarkup('Render an icon from UI Icons module.'),
+  prop_types: ['slot']
+)]
+class IconRenderableSource extends IconSource {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropValue(): mixed {
+    $value = $this->getSetting('value');
+    if (!$value) {
+      return [];
+    }
+    [$iconset_id, $icon_id] = explode(':', $value['icon_id']);
+    return [
+      "#type" => "icon",
+      "#iconset" => $iconset_id ?? '',
+      "#icon" => $icon_id ?? '',
+      "#options" => $value['settings'] ?? [],
+    ];
+  }
+
+}
