e402a50d6569fd38f4d1e07c95eb4d0c45dca03f
Tim Diels <41869-timdiels1@users.noreply.drupalcode.org>
2026-02-27 10:13:13 +0100

    Remove checks on module as we're already in the module and plugin can only be used if module is enabled

--- modules/ui_icons_field/src/Plugin/UiPatterns/Source/LinkIconSource.php
+++ modules/ui_icons_field/src/Plugin/UiPatterns/Source/LinkIconSource.php
@@ -34,13 +34,6 @@ class LinkIconSource extends SourcePluginPropValue {
    * {@inheritdoc}
    */
   public function getPropValue(): array {
-    if (!$this->moduleHandler->moduleExists('ui_icons_field')) {
-      return [];
-    }
-    elseif (!$this->moduleHandler->moduleExists('ui_icons_field_link_attributes')) {
-      return [];
-    }
-
     $field_items = $this->getContextValue("ui_patterns:field:items");
     $field_index = $this->getContextValue("ui_patterns:field:index");
 
@@ -62,34 +55,6 @@ class LinkIconSource extends SourcePluginPropValue {
     }
 
     return [];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state): array {
-    $form = parent::settingsForm($form, $form_state);
-
-    if (!$this->moduleHandler->moduleExists('ui_icons_field')) {
-      $error_message = $this->t('You need to enable the UI Icons Fields module to use this source.');
-    }
-    elseif (!$this->moduleHandler->moduleExists('ui_icons_field_link_attributes')) {
-      $error_message = $this->t('You need to enable the UI Icons Link Attributes module to use this source.');
-    }
-
-    if (isset($error_message)) {
-      $form['warning'] = [
-        '#theme' => 'status_messages',
-        '#message_list' => [
-          'error' => [
-            $error_message,
-          ],
-        ],
-      ];
-      return $form;
-    }
-
-    return $form;
   }
 
 }
